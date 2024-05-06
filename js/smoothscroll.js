5918082afb54a6c69681f7274416e00a710582e5
John Ferreira <42903558+SkayJ@users.noreply.github.com>
2024-05-05 21:24:04 -0400

    Delete js directory

--- js/smoothscroll.js
+++
@@ -1,7 +0,0 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-// create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-  smooth: 2,   // seconds it takes to "catch up" to native scroll position
-  effects: true // look for data-speed and data-lag attributes on elements and animate accordingly
-});
